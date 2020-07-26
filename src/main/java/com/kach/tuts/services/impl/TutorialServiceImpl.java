6562224c1fad411ef26124c0c9d659e4806b6524
kachmazoff <33556235+kachmazoff@users.noreply.github.com>
2020-07-27 00:44:12 +0400

    Revert "Revert "develop-tutorial""

--- src/main/java/com/kach/tuts/services/impl/TutorialServiceImpl.java
+++ src/main/java/com/kach/tuts/services/impl/TutorialServiceImpl.java
@@ -1,44 +1,45 @@
 package com.kach.tuts.services.impl;
 
-import com.kach.tuts.dao.TutorialStepDao;
-import com.kach.tuts.domain.TutorialStep;
-import com.kach.tuts.services.TutorialStepService;
+
+import com.kach.tuts.dao.TutorialDao;
+import com.kach.tuts.domain.Tutorial;
+import com.kach.tuts.services.TutorialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TutorialServiceImpl implements TutorialStepService {
-    private TutorialStepDao tutorialStepDao;
+public class TutorialServiceImpl implements TutorialService {
+    private TutorialDao tutorialDao;
 
     @Autowired
-    public void setTutorialStepDao(TutorialStepDao tutorialStepDao) {
-        this.tutorialStepDao = tutorialStepDao;
+    public void setHashtagDao(TutorialDao tutorialDao) {
+        this.tutorialDao = tutorialDao;
     }
 
     @Override
-    public TutorialStep save(TutorialStep tutorialStep) {
-        return this.tutorialStepDao.save(tutorialStep);
+    public Tutorial save(Tutorial tutorial) {
+        return tutorialDao.save(tutorial);
     }
 
     @Override
-    public void update(TutorialStep tutorialStep) {
-        this.tutorialStepDao.update(tutorialStep);
+    public void update(Tutorial tutorial) {
+        tutorialDao.update(tutorial);
     }
 
     @Override
-    public TutorialStep getById(Long id) {
-        return this.tutorialStepDao.getById(id);
+    public Tutorial getById(Long id) {
+        return tutorialDao.getById(id);
     }
 
     @Override
-    public List<TutorialStep> getAll() {
-        return this.tutorialStepDao.getAll();
+    public List<Tutorial> getAll() {
+        return tutorialDao.getAll();
     }
 
     @Override
-    public void delete(TutorialStep tutorialStep) {
-        this.tutorialStepDao.delete(tutorialStep);
+    public void delete(Tutorial hashtag) {
+        tutorialDao.delete(hashtag);
     }
 }
